32ed174ca64b91a7fdcac5b33fc0556a27439241
logvinov Mikhail <mlogvinov2016@gmail.com>
2020-09-24 11:56:20 +0300

    test-pravki

--- 04_function.js
+++ 04_function.js
@@ -26,46 +26,46 @@
 // }, 1000)
 
 // 3 Стрелочные функции
-function greet() {
-  console.log('Привет - ')
-}
+// function greet() {
+//   console.log('Привет - ')
+// }
 
-const arrow = (name, age) => {
-  console.log('Привет - ', name, age)
-}
+// const arrow = (name, age) => {
+//   console.log('Привет - ', name, age)
+// }
 
-const arrow2 = name => console.log('Привет - ', name)
+// const arrow2 = name => console.log('Привет - ', name)
 
 // arrow2('Vladilen')
 
-const pow2 = num => num ** 2
+// const pow2 = num => num ** 2
 
 // console.log(pow2(5))
 
 // 4 Параметры по умолчанию
-const sum = (a = 40, b = a * 2) => a + b
+// const sum = (a = 40, b = a * 2) => a + b
 
 // console.log(sum(41, 4))
 // console.log(sum())
 
-function sumAll(...all) {
-  let result = 0
-  for (let num of all) {
-    result += num
-  }
-  return result
-}
+// function sumAll(...all) {
+//   let result = 0
+//   for (let num of all) {
+//     result += num
+//   }
+//   return result
+// }
 
-const res = sumAll(1, 2, 3, 4, 5)
+// const res = sumAll(1, 2, 3, 4, 5)
 // console.log(res)
 
 // 5 Замыкания
-function createMember(name) {
-  return function(lastName) {
-    console.log(name + lastName)
-  }
-}
+// function createMember(name) {
+//   return function(lastName) {
+//     console.log(name + lastName)
+//   }
+// }
 
-const logWithLastName = createMember('Vladilen')
-console.log(logWithLastName('Minin'))
-console.log(logWithLastName('Kuznezov'))
+// const logWithLastName = createMember('Vladilen')
+// console.log(logWithLastName('Minin'))
+// console.log(logWithLastName('Kuznezov'))
